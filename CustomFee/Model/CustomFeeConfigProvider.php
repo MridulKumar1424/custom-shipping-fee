bfbdf50f228fd40f2907bc10bb870fd70050cc71
Mridul Kumar <iammridul2000@gmail.com>
2022-11-23 18:30:14 +0530

    Add files via upload

---
+++ CustomFee/Model/CustomFeeConfigProvider.php
@@ -0,0 +1,56 @@
+<?php
+namespace Mridul\CustomFee\Model;
+
+use Magento\Checkout\Model\ConfigProviderInterface;
+
+class CustomFeeConfigProvider implements ConfigProviderInterface
+{
+    /**
+     * @var \Mridul\CustomFee\Helper\Data
+     */
+    protected $dataHelper;
+
+    /**
+     * @var \Magento\Checkout\Model\Session
+     */
+    protected $checkoutSession;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @param \Mridul\CustomFee\Helper\Data $dataHelper
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(
+        \Mridul\CustomFee\Helper\Data $dataHelper,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Psr\Log\LoggerInterface $logger
+
+    )
+    {
+        $this->dataHelper = $dataHelper;
+        $this->checkoutSession = $checkoutSession;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig()
+    {
+        $customFeeConfig = [];
+        $enabled = $this->dataHelper->isModuleEnabled();
+        $minimumOrderAmount = $this->dataHelper->getMinimumOrderAmount();
+        $customFeeConfig['fee_label'] = $this->dataHelper->getFeeLabel();
+        $quote = $this->checkoutSession->getQuote();
+        $subtotal = $quote->getSubtotal();
+        $customFeeConfig['custom_fee_amount'] = $this->dataHelper->getCustomFee();
+        $customFeeConfig['show_hide_customfee_block'] = ($enabled && ($minimumOrderAmount <= $subtotal) && $quote->getFee()) ? true : false;
+        $customFeeConfig['show_hide_customfee_shipblock'] = ($enabled && ($minimumOrderAmount <= $subtotal)) ? true : false;
+        return $customFeeConfig;
+    }
+}
---
+++ CustomFee/Plugin/Checkout/Model/ShippingInformationManagement.php
@@ -0,0 +1,51 @@
+<?php
+namespace Mridul\CustomFee\Plugin\Checkout\Model;
+
+
+class ShippingInformationManagement
+{
+    /**
+     * @var \Magento\Quote\Model\QuoteRepository
+     */
+    protected $quoteRepository;
+
+    /**
+     * @var \Mridul\CustomFee\Helper\Data
+     */
+    protected $dataHelper;
+
+    /**
+     * @param \Magento\Quote\Model\QuoteRepository $quoteRepository
+     * @param \Mridul\CustomFee\Helper\Data $dataHelper
+     */
+    public function __construct(
+        \Magento\Quote\Model\QuoteRepository $quoteRepository,
+        \Mridul\CustomFee\Helper\Data $dataHelper
+    )
+    {
+        $this->quoteRepository = $quoteRepository;
+        $this->dataHelper = $dataHelper;
+    }
+
+    /**
+     * @param \Magento\Checkout\Model\ShippingInformationManagement $subject
+     * @param $cartId
+     * @param \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+     */
+    public function beforeSaveAddressInformation(
+        \Magento\Checkout\Model\ShippingInformationManagement $subject,
+        $cartId,
+        \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+    )
+    {
+        $customFee = $addressInformation->getExtensionAttributes()->getFee();
+        $quote = $this->quoteRepository->getActive($cartId);
+        if ($customFee) {
+            $fee = $this->dataHelper->getCustomFee();
+            $quote->setFee($fee);
+        } else {
+            $quote->setFee(NULL);
+        }
+    }
+}
+
